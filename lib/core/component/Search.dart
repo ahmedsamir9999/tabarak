3982aafcc429a2a0847b5f4b35adab8d58ffe7dd
abdelrahman ashraf <63114136+abdoashraf111@users.noreply.github.com>
2023-11-13 16:50:09 +0200

    abdo

---
+++ lib/core/component/Search.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tabarak/App/presentation/controller/home_provider.dart';
+import 'package:quran/quran.dart' as quran;
+class Search extends StatelessWidget {
+   Search({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<HomeProvider>(
+      builder: (BuildContext context, pro, Widget? child)
+      {return Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 100,
+                child: TextFormField(
+                  onChanged: (value) {
+                    pro.test(value);
+                    print(pro.showList);
+                      // pro.showList=pro.suraList.where((element) => element.contains(value),).toList();
+
+
+                    // pro.showList=pro.suraList;
+                  },
+                ),
+              ),
+              // ListView.builder(
+              //   itemBuilder: (context, index) =>
+              //       Text(pro.suraList as String)
+              //   ,)
+              Text(pro.ttt,style: const TextStyle(decorationColor: Colors.black,fontSize: 50),),
+              TextButton(onPressed: (){
+                pro.saveList();
+
+                print(pro.suraList);
+                },
+                  child: Text("Button"))
+            ],
+          ),
+        ),
+      ); },
+
+    );
+  }
+}
